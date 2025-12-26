222622a8aad3a9c2a3e165c761ebc3baae412e10
Durga prasad <vasamsettydurgaprasad22@gmail.com>
2025-12-27 01:33:29 +0530

    Fix Django settings and static files for Render

--- SSSIT/settings.py
+++ SSSIT/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com',]
+ALLOWED_HOSTS = ["*"]
 
 
 
